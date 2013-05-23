7abeb315932c84b1df0b12f72a9be93e52ccf413
JustinP <booooo201@gmail.com>
2013-05-23 14:21:36 -0400

    More work on opening files.

--- src/com/github/oxidedevelopment/privatenotebook/utils/NotebookDirectory.java
+++ src/com/github/oxidedevelopment/privatenotebook/utils/NotebookDirectory.java
@@ -1,5 +1,6 @@
 package com.github.oxidedevelopment.privatenotebook.utils;
 
+import javax.swing.*;
 import java.io.File;
 
 /**
@@ -10,19 +11,26 @@ import java.io.File;
 public class NotebookDirectory {
 
     File notebookDirectory;
+    File savePath;
 
     public NotebookDirectory(File notebookDirectory, boolean setup) {
         this.notebookDirectory = notebookDirectory;
         if(!setup)
             setupDirectoryTree();
+
+        savePath = new File(notebookDirectory.getAbsolutePath() + System.getProperty("file.separator") + "saves");
     }
 
     private void setupDirectoryTree() {
         File saves = new File(notebookDirectory.getAbsolutePath() + System.getProperty("file.separator") + "saves");
-        if(!saves.exists()) saves.mkdir();
+        saves.mkdir();
     }
 
-    public String openSave(File save){
+    public String openSave(){
+        JFileChooser dlg = new JFileChooser(savePath);
+        if(dlg.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+            JOptionPane.showMessageDialog(null, "Approved");
+        }
         return null;
     }
 }

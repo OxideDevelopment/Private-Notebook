495e44eac0cd4410faf6a4d9ee89024a167ed211
JustinP <booooo201@gmail.com>
2013-05-23 14:19:40 -0400

    Added Open, and changed font.

--- src/com/github/oxidedevelopment/privatenotebook/windows/NotebookEditor.java
+++ src/com/github/oxidedevelopment/privatenotebook/windows/NotebookEditor.java
@@ -1,7 +1,11 @@
 package com.github.oxidedevelopment.privatenotebook.windows;
 
+import com.github.oxidedevelopment.privatenotebook.PrivateNotebook;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by: Justin
@@ -10,29 +14,60 @@ import java.awt.*;
  */
 public class NotebookEditor extends JFrame {
 
-    JPanel menuPanel;
     JMenuBar menu;
+    JMenu fileMenu;
     JPanel textPanel;
-    JTextArea editor;
+    JTextPane editor;
 
     public NotebookEditor() {
         super("Private Notebook");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
         createGui();
     }
 
     private void createGui() {
-        menuPanel = new JPanel();
 
+        //Menu bar
         menu = new JMenuBar();
+        //File menubar object
+        fileMenu = new JMenu("File");
+        //Setting the menubar
         setJMenuBar(menu);
-        menuPanel.add(menu);
+        //Adding the file item to menubar
+        JMenuItem file_open = new JMenuItem("Open");
+        file_open.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openFile();
+            }
+        });
+
+        fileMenu.add(file_open);
+        menu.add(fileMenu);
 
         textPanel = new JPanel();
-        editor = new JTextArea();
-        menuPanel.add(editor);
+        textPanel.setLayout(new BorderLayout());
+        editor = new JTextPane();
+        editor.setPreferredSize(new Dimension(590, 400 - menu.getHeight()));
+        //editor.setLayout(null);
+        editor.setMinimumSize(new Dimension(0, 0));
+        editor.setFont(new Font("Segoe UI", Font.PLAIN, 12));
 
-        add(menuPanel, BorderLayout.NORTH);
+        JScrollPane scrollPane = new JScrollPane(editor);
+        scrollPane.createHorizontalScrollBar();
+        scrollPane.createVerticalScrollBar();
+
+        textPanel.add(scrollPane);
+
         add(textPanel, BorderLayout.CENTER);
+
+
+    }
+
+    private void openFile() {
+        PrivateNotebook.notebookDirectory.openSave();
     }
 
 
